13c134bd84ab3507d50e15c628ce052551052841
Chandanagunaga <94128160+Chandanagunaga@users.noreply.github.com>
2022-08-24 15:35:33 +0530

    Add files via upload

---
+++ ConsoleApp1/countwords.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class countwords
+    {
+        public static void Main()
+        {
+            int a = 0, myWord = 1;
+            string str = "Digital Engineer Trainee in Sonata Software Limited";
+            while (a <= str.Length - 1)
+            {
+                if (str[a] == ' ' || str[a] == '\n' || str[a] == '\t')
+                {
+                    myWord++;
+                }
+                a++;
+            }
+            Console.Write("Number of words in the string = {0}\n", myWord);
+        }
+    }
+}
+    
